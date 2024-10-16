a6587ae3de27eed2b5f6ecae576a7dcdfe4f4a6e
sajidh <sajidhshaji4@gmail.com>
2024-10-16 23:30:15 +0530

    bottom sheet finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:testing_widgets/connection_checker/internet_checker_app.dart';
+import 'package:testing_widgets/profile/mortage.dart';
+import 'package:testing_widgets/profile/real_estate_profile.dart';
 import 'package:testing_widgets/whatsApp_message/vibration_checking_screen.dart';
 
 void main() {
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const VibrationCheckingScreen(),
+      home: const MortgagePage(),
     );
   }
 }
